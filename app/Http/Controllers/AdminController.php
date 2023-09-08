58cc423fd40c79ef724189f2088e745df38288e8
Kushal <kushalobroy@gmail.com>
2023-09-08 10:50:56 +0530

    intial commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,56 +10,79 @@ use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
 {
-    public function create(){
-        $data=Category::all();
-        return view('create',compact('data'));
+    public function create()
+    {
+        $data = Category::all();
+        return view('create', compact('data'));
     }
-     public function createProduct(){
+    public function createProduct()
+    {
         $data = Category::all();
         return view('createProduct', compact('data'));
     }
-    public function createCategory(Request $request){
+    public function createCategory(Request $request)
+    {
         $request->validate([
-            'name'=>'required',
+            'name' => 'required',
         ]);
-       
-        Category::create($request->post());
-        Session::flash('category_add','Category Created Successfully');
-        return redirect()->route('create');
 
-    }   
-    public function createProducts(Request $request){
+        Category::create($request->post());
+        Session::flash('category_add', 'Category Created Successfully');
+        return redirect()->route('create');
+    }
+    public function createProducts(Request $request)
+    {
         $request->validate([
-            'name'=>'required',
-            'category_id'=>'required',
+            'name' => 'required',
+            'category_id' => 'required',
         ]);
-       Session::flash('product_add','Product Added Succesfully');
+        Session::flash('product_add', 'Product Added Succesfully');
         Product::create($request->post());
         return redirect()->route('home');
-
-    } 
-    public function updateProduct($id){
+    }
+    public function updateProduct($id)
+    {
         $data = Product::find($id);
         $da = Category::all();
-        return view('updateProduct',compact('data','da'));
-    } 
-    public function updateProducts(Request $request,$id){
-        $prod=Product::find($id);
+        return view('updateProduct', compact('data', 'da'));
+    }
+    public function updateProducts(Request $request, $id)
+    {
+        $prod = Product::find($id);
         $prod->name = $request->name;
         $prod->category_id = $request->category_id;
         $prod->save();
-        Session::flash('update_message','Updated Successfully');
+        Session::flash('update_message', 'Updated Successfully');
         return redirect()->route('home');
-    } 
-    public function delete($id){
+    }
+    public function delete($id)
+    {
         Product::destroy($id);
         Session::flash('delete_message', 'Deleted Successfully');
         return back();
     }
-    public function deleteCategory($id){
+    public function deleteCategory($id)
+    {
         Category::destroy($id);
         Session::flash('delete_message', 'Deleted Successfully');
         return back();
     }
-    
+    public function update(Request $request, $id)
+    {
+        $request->validate(
+            [
+                'name' => 'required|max:191',
+                'category_id' => 'required|max:191',
+            ]
+        );
+        $prod = Product::find($id);
+        if ($prod) {
+            $prod->name = $request->name;
+            $prod->category_id = $request->category_id;
+            $prod->update();
+            return response()->json(['message' => 'Product Updated Successfully'], 200);
+        } else {
+            return response()->json(['message' => 'No Product found'], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AdminController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::put('product/{id}/update',[AdminController::class,'update']);
